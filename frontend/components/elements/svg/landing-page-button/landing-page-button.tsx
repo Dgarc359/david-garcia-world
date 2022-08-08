27519b092c31e2fdc67d9feeb357f9918a7e4fbb
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2022-08-07 21:03:49 -0400

    add .env.template, start landing page, add landing page to router

---
+++ frontend/components/elements/svg/landing-page-button/landing-page-button.tsx
@@ -0,0 +1,27 @@
+
+type strokes = "seagreen";
+
+interface LandingPageButtonProps {
+  stroke: strokes;
+}
+
+export const LandingPageButton = (props: LandingPageButtonProps) => {
+
+  const { stroke } = props;
+
+  return (
+    <>
+      <svg
+        viewBox="0 0 1 1"
+      >
+        <circle
+          r={10}
+          strokeWidth={2}
+          stroke={stroke}
+          cx={10}
+          cy={10}
+        ></circle>
+      </svg>
+    </>
+  );
+}
--- frontend/components/router/router.tsx
+++ frontend/components/router/router.tsx
@@ -6,14 +6,16 @@ import {
 } from "react-router-dom";
 import { Resume } from "../resume/resume";
 import { Example } from "../example/example";
+import { LandingPage } from "../landing-page/landing-page";
 
 export const RouterComponent = () => {
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/" element={<Example />} />
-          <Route path="/resume" element={<Resume />} />
+          <Route path="/example" element={ <Example/> } />
+          <Route path="/resume" element={ <Resume/> } />
+          <Route path="/" element={ <LandingPage/> } />
         </Switch>
       </div>
     </Router>
