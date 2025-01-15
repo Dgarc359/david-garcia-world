bb6a42137801541506896fc46c98c90126eb3483
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2025-01-14 20:38:17 -0500

    add routing for nextjs function, adjust slug page to try to fix routing issue

--- frontend/app/projects/[slug]/page.tsx
+++ frontend/app/projects/[slug]/page.tsx
@@ -5,25 +5,28 @@ import { useRepositoryReadme } from "@/app/lib/util";
 
 export default function ProjectsPage() {
   console.log("loading projects page");
-  const pathName = usePathname()
-    .split("/")
-    .filter((s) => s !== "projects")
-    .join("");
 
-  const {
-    data: readmeData,
-    error: readmeError,
-    isLoading: readmeIsLoading,
-  } = useRepositoryReadme("Dgarc359", pathName);
+  return <div>Hello WORLD</div>
 
-  if (readmeError && readmeError.status)
-    return <div> An error occured! {JSON.stringify(readmeError)} </div>;
-  if (readmeIsLoading) return <div> Still loading! </div>;
-  if (readmeData !== undefined) {
-    return (
-      <div className="flex justify-center">
-      <MarkdownC payload={readmeData} customStyles={"w-2/3"}/>
-      </div>
-    );
-  }
+  // const pathName = usePathname()
+  //   .split("/")
+  //   .filter((s) => s !== "projects")
+  //   .join("");
+
+  // const {
+  //   data: readmeData,
+  //   error: readmeError,
+  //   isLoading: readmeIsLoading,
+  // } = useRepositoryReadme("Dgarc359", pathName);
+
+  // if (readmeError && readmeError.status)
+  //   return <div> An error occured! {JSON.stringify(readmeError)} </div>;
+  // if (readmeIsLoading) return <div> Still loading! </div>;
+  // if (readmeData !== undefined) {
+  //   return (
+  //     <div className="flex justify-center">
+  //     <MarkdownC payload={readmeData} customStyles={"w-2/3"}/>
+  //     </div>
+  //   );
+  // }
 }
